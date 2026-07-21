22f183c6ab22a31e059c64bacf374975aac1f09f
quocquoc123 <quocphamvan711@gmail.com>
2026-07-21 13:33:54 +0700

    update

--- QLBanDoAnNhanh/QLBanDoAnNhanh/obj/Debug/net8.0/QLBanDoAnNhanh.AssemblyInfo.cs
+++ QLBanDoAnNhanh/QLBanDoAnNhanh/obj/Debug/net8.0/QLBanDoAnNhanh.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QLBanDoAnNhanh")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a81c57eb5545428f45860f6f90cfa6b71d39d21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+588195ab38443367d0c6339b18c5c8e3b2ff9d06")]
 [assembly: System.Reflection.AssemblyProductAttribute("QLBanDoAnNhanh")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QLBanDoAnNhanh")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
